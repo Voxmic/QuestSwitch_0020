c0846fa1224ef363406469daf1607a1e8f73c1a6
Voxmic <farelsurya16@gmail.com>
2025-11-04 22:07:43 +0700

    menambahkan system out print dan memanggil variabel untuk memasukan nilai

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -5,7 +5,7 @@ public class QuestSwitch {
     public static void main(String[] args) {
         
         Scanner ns = new Scanner(System.in);
-        
+
         double nilaiSD;
         double nilaiPBO;
         double nilaiBD;
@@ -14,11 +14,19 @@ public class QuestSwitch {
         double rataRata;
 
         System.out.println("Seleksi Asisten Dosen");
-        System.out.print("Masukkan Nilai Mata Kuliah Anda : ");
-        int nilai = ns.nextInt();
-        String grade;
-
-
+        System.out.println("Masukkan Nilai Mata Kuliah Anda.");
+        System.out.print("Nilai Struktur Data : ");
+        nilaiSD = ns.nextDouble();
+        System.out.print("Nilai Pemrograman Berorientasi Objek : ");
+        nilaiPBO = ns.nextDouble();
+        System.out.print("Nilai Basis Data : ");
+        nilaiBD = ns.nextDouble();
+        System.out.print("Nilai Algoritma dan Pemrograman : ");
+        nilaiAP = ns.nextDouble();
+        System.out.print("Nilai Rekayasa Perangkat Lunak : ");
+        nilaiRPL = ns.nextDouble();
+        rataRata = (nilaiSD + nilaiPBO + nilaiBD + nilaiAP + nilaiRPL) / 5;
+        System.out.println("Rata-rata Nilai Anda adalah : " + rataRata);
     }
     
 }
