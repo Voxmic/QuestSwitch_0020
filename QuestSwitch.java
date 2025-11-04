ce8c70028b6635e9174f36396c2b2359351d33ad
Voxmic <farelsurya16@gmail.com>
2025-11-04 23:49:50 +0700

    mencari rata-rata dan ipk

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -12,6 +12,7 @@ public class QuestSwitch {
         double nilaiAP;
         double nilaiRPL;
         double rataRata;
+        double IPK;
 
         System.out.println("Seleksi Asisten Dosen");
         System.out.println("Masukkan Nilai Mata Kuliah Anda.");
@@ -27,6 +28,28 @@ public class QuestSwitch {
         nilaiRPL = ns.nextDouble();
         rataRata = (nilaiSD + nilaiPBO + nilaiBD + nilaiAP + nilaiRPL) / 5;
         System.out.println("Rata-rata Nilai Anda adalah : " + rataRata);
+        IPK = rataRata / 25;
+        System.out.println("IPK Anda adalah : " + IPK);
+
+        if (IPK > 3.75) {
+            System.out.println("Predikat Nilai Anda : A (Sangat Baik)");
+        }
+        else if (IPK >= 3.50 || IPK <= 3.74) {
+            System.out.println("Predikat Nilai Anda : AB (Baik Sekali)");
+        }
+        else if (IPK >= 3.00 || IPK <= 3.49) {
+            System.out.println("Predikat Nilai Anda : B (Baik)");
+        }
+        else if (IPK >= 2.50 || IPK <= 2.99) {
+            System.out.println("Predikat Nilai Anda : BC (Cukup)");
+        }
+        else if (IPK >= 2.00 || IPK <= 2.49) {
+            System.out.println("Predikat Nilai Anda : C (Kurang)");
+        }
+        else {
+            System.out.println("Nilai Tidak Valid");
+        }
+       
     }
     
 }
