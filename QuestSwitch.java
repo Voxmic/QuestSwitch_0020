7bd012d5e601c3f45886f2991d4e99a7a887b9e1
Voxmic <farelsurya16@gmail.com>
2025-11-04 21:29:35 +0700

    menambahkan scanner dan variabel

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class QuestSwitch {
+    
     public static void main(String[] args) {
         
+        Scanner ns = new Scanner(System.in);
+        
+        double nilaiSD;
+        double nilaiPBO;
+        double nilaiBD;
+        double nilaiAP;
+        double nilaiRPL;
+        double rataRata;
+
+        System.out.println("Seleksi Asisten Dosen");
+        System.out.print("Masukkan Nilai Mata Kuliah Anda : ");
+        int nilai = ns.nextInt();
+        String grade;
+
+
     }
     
 }
